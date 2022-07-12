d512db77d47ce19e5dc3273e28f88d08c5ad8a08
grudik <grudik@ukr.net>
2022-07-13 00:21:57 +0300

    Add project files.

---
+++ DataContext/ToDoDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Models;
+
+namespace WebAPIServer.DataContext
+{
+    public class ToDoDatabaseContext : DbContext
+    {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        public ToDoDatabaseContext(DbContextOptions<ToDoDatabaseContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
